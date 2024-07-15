5b59dd3b2b69fb1ef8a2cb6d68c8418b5d43b8bf
Kurt Secretario <secretariokurt3219@gmail.com>
2024-07-15 16:47:59 +0800

    <class>(utility) : add schedule class

---
+++ src/TimeSchedule.java
@@ -0,0 +1,60 @@
+import java.time.LocalTime;
+import java.util.Objects;
+import java.util.TreeMap;
+
+public class TimeSchedule {
+    private static final TreeMap<Integer, Schedule> timeSchedules = new TreeMap<>();
+
+    public static final int MIN_INDEX = 1;
+    public static final int MAX_INDEX = 7;
+
+    public static class Schedule {
+        LocalTime startTime;
+        LocalTime endTime;
+
+        public Schedule(final LocalTime START_TIME, final LocalTime END_TIME) {
+            this.startTime = START_TIME;
+            this.endTime = END_TIME;
+        }
+
+        @Override
+        public String toString() {
+            return "Start = " + this.startTime + ", End = " + this.endTime;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            Schedule schedule = (Schedule) o;
+            return Objects.equals(startTime, schedule.startTime) &&
+                    Objects.equals(endTime, schedule.endTime);
+        }
+    }
+
+    static {
+        timeSchedules.put(MIN_INDEX, new Schedule(LocalTime.of(9, 0), LocalTime.of(10, 0)));
+        timeSchedules.put(2, new Schedule(LocalTime.of(9, 0), LocalTime.of(10, 0)));
+        timeSchedules.put(3, new Schedule(LocalTime.of(10, 0), LocalTime.of(11, 0)));
+        timeSchedules.put(4, new Schedule(LocalTime.of(11, 0), LocalTime.of(12, 0)));
+        timeSchedules.put(5, new Schedule(LocalTime.of(13, 0), LocalTime.of(14, 0)));
+        timeSchedules.put(6, new Schedule(LocalTime.of(14, 0), LocalTime.of(15, 0)));
+        timeSchedules.put(MAX_INDEX, new Schedule(LocalTime.of(15, 0), LocalTime.of(16, 0)));
+    }
+
+    public static Schedule getSchedule(final int INDEX) {
+        return timeSchedules.getOrDefault(INDEX, null);
+    }
+
+    public static void printSchedule() {
+        for (Integer key : timeSchedules.keySet()) {
+            System.out.println("[" + key + "] " + timeSchedules.get(key));
+        }
+    }
+
+
+}
---
+++ src/User.java
@@ -0,0 +1,127 @@
+import java.util.Objects;
+
+public class User extends UserDb {
+    private String name;
+    private String email;
+    private String password;
+    private Reservation reservation;
+
+    public User() {
+        this.name = null;
+        this.email = null;
+        this.password = null;
+        this.reservation = null;
+    }
+
+    public User(final String NAME, final String EMAIL, final String PASSWORD, final Reservation RESERVATION) {
+        this.name = NAME;
+        this.email = EMAIL;
+        this.password = PASSWORD;
+        this.reservation = RESERVATION;
+    }
+
+    public static String inputName() {
+        System.out.print("Enter your name: (LN, FN, MI.)");
+        return InputReader.readString();
+    }
+
+    public static String inputEmail() {
+        System.out.print("Enter your email: ");
+        return InputReader.readString();
+    }
+
+    public static String inputPassword() {
+        System.out.print("Enter your password: ");
+        return InputReader.readString();
+    }
+
+    public final Boolean setName(final String NAME) {
+        if (NAME.length() < 5 || NAME.length() > 100) {
+            System.out.println("Entered name length must be between 5 and 100!");
+            return false;
+        }
+
+        this.name = NAME;
+        return true;
+    }
+
+    public final Boolean setEmail(final String EMAIL) {
+        if (EMAIL.length() < 5 || EMAIL.length() > 50) {
+            System.out.println("Entered email length must be between 5 and 50!");
+            return false;
+        }
+
+        if (!EMAIL.contains("@")) {
+            System.out.println("Incorrect email address!");
+            return false;
+        }
+
+        if (findUser(EMAIL) != null) {
+            System.out.println("Email address already exists!");
+            return false;
+        }
+
+        this.email = EMAIL;
+        return true;
+    }
+
+    public final Boolean setPassword(final String PASSWORD) {
+        if (PASSWORD.length() < 5 || PASSWORD.length() > 50) {
+            System.out.println("Entered password length must be between 5 and 50!");
+            return false;
+        }
+
+        this.password = PASSWORD;
+        return true;
+    }
+
+    public final Boolean setReservation(final Reservation RESERVATION) {
+        if (ReservationDb.findReservation(RESERVATION)) {
+            System.out.println("Reservation already exists!");
+            return false;
+        }
+
+        this.reservation = RESERVATION;
+        return true;
+    }
+
+    public final String getName() {
+        return this.name;
+    }
+
+    public final String getEmail() {
+        return this.email;
+    }
+
+    public final String getPassword() {
+        return this.password;
+    }
+
+    public final Reservation getReservation() {
+        return this.reservation;
+    }
+
+    @Override
+    public final String toString() {
+        return "Name = '" + name + '\'' +
+                ", Email = '" + email + '\'' +
+                ", Password = '" + password + '\'';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        User user = (User) o;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(email);
+    }
+}
