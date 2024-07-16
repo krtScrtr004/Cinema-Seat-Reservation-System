78ab76012b1582de0b5f5daeb71f7658125ff386
Kurt Secretario <secretariokurt3219@gmail.com>
2024-07-16 15:51:58 +0800

    <class>(utility) : refactor ReservationDb class to a hashmap

--- src/ReservationDb.java
+++ src/ReservationDb.java
@@ -1,27 +1,52 @@
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReservationDb {
-    private static HashSet<Reservation> reservationDb = new HashSet<>();
+    private static HashMap<TimeSchedule.Schedule, ArrayList<Reservation>> reservationDb = new HashMap<TimeSchedule.Schedule, ArrayList<Reservation>>();
 
-    public static void addReservation(final Reservation RESERVATION) {
-        reservationDb.add(RESERVATION);
+    public static void addReservation(final TimeSchedule.Schedule SCHEDULE, final Reservation RESERVATION) {
+        // check if particular schedule is already win the database
+        if (!reservationDb.containsKey(SCHEDULE))
+            reservationDb.put(SCHEDULE, new ArrayList<>());
+
+        ArrayList<Reservation> tempArray = reservationDb.get(SCHEDULE);
+        if (tempArray != null)
+            tempArray.add(RESERVATION);
     }
 
-    public static void deleteReservation(final Reservation RESERVATION) {
-        if (!reservationDb.contains(RESERVATION)) {
+    public static void deleteReservation(final TimeSchedule.Schedule SCHEDULE, final Reservation RESERVATION) {
+        if (!reservationDb.contains(SCHEDULE)) {
             System.out.println("Reservation not found.");
             return;
         }
 
-        reservationDb.remove(RESERVATION);
+        ArrayList<Reservation> tempArray = reservationDb.get(SCHEDULE);
+        if (tempArray != null)
+            tempArray.remove(RESERVATION);
     }
 
-    public static Boolean findReservation(final Reservation RESERVATION) {
-        return reservationDb.contains(RESERVATION);
+    public static ArrayList<Reservation> findReservation(final TimeSchedule.Schedule SCHEDULE, final Reservation RESERVATION) {
+        if (!reservationDb.contains(SCHEDULE)) {
+            System.out.println("Reservation not found.");
+            return null;
+        }
+
+        ArrayList<Reservation> tempArray = reservationDb.get(SCHEDULE);
+        if (tempArray != null)
+            Collections.sort(tempArray);
+        final int INDEX = tempArray.indexOf(RESERVATION);
+        return ((INDEX > 0) ? tempArray : null);
     }
+
 
     public static void displayAllReservations() {
-        for (Reservation reservation : reservationDb)
-            System.out.println(reservation);
+       for (Map.Entry<TimeSchedule.Schedule, ArrayList<Reservation>> entry : reservationDb.entrySet()) {
+            ArrayList<Reservation> tempArray = entry.getValue();
+            System.out.println(entry.getKey());
+            for (Reservation reservation : tempArray)
+                System.out.println(reservation);
+        }
     }
 }
